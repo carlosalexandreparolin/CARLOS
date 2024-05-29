fa0ff699f00436184a79f842a83604afe2fa9ca4
carlosalexandreparolin <125028046+carlosalexandreparolin@users.noreply.github.com>
2024-05-29 08:05:59 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,21 @@
 const caixaPrincipal = document.querySelector(".caixa-principal")
+const caixaPerguntas = document.querySelector(".caixa-perguntas")
+const caixaAlternativas = document.querySelector(".caixa-alternativa")
+const caixaReposta = document.querySelector(".caixa-resosta")
+const textoResposta = document.querySelector(".resposta")
+const perguntas = [
+    {
+        enunciado: "Pergunta 1",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    },
+    {
+        enunciado: "Pergunta 2",
+        alternativas: [
+            "Alternativa 1",
+            "Alternativa 2"
+        ]
+    }
+];
