6bac3fc2f440b0091dcf38d9080df4ec7da706ea
carlosalexandreparolin <125028046+carlosalexandreparolin@users.noreply.github.com>
2024-09-04 08:03:18 -0300

    Delete js directory

--- js/aleatório.js
+++
@@ -1,4 +0,0 @@
-export function aleatorio (lista){
-    const posicao = Math.floot(Math.random()* lista.length);
-    return lista[posicao];
-}
--- js/dadod6.js
+++
@@ -1,6 +0,0 @@
-function dadoD6() {
-    var numero = Math.floor(Math.random() * 6);
-    return numero;
-}
-
-console.log(dadoD6());
